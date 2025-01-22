f0c31fbfae790880b8f76a078ada2d17ebe626d2
alvaro <alvaro.torres.chacon@colegiolitterator.com>
2025-01-22 17:05:38 +0100

    Inicialización del marco principal, los paneles y los botones.

--- calculator_java/src/calculator_java/Enginee.java
+++ calculator_java/src/calculator_java/Enginee.java
@@ -1,5 +1,27 @@
 package calculator_java;
 
-public class Enginee {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-}
+	public abstract class Enginee implements ActionListener {
+	   /** Marco principal, paneles y botones */
+		private JFrame frame;
+	    private JPanel contentPanel;
+	    private JPanel displayPanel;
+	    private JPanel buttonPanel;
+	    private JTextField display;
+	   	private JButton n0, n1, n2, n3, n4, n5, n6, n7, n8, n9;
+	    private JButton divide, multiply, subtract, add, equal, reset;
+	    private int num1;
+	    private int num2;
+	    private int result;
+	    private char operation;
+	    
+	    
+	}
